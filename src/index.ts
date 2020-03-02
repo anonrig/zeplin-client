e72187c9311184768c0bda7b4d40e41bf1f3e7ef
Yagiz Nizipli <yagiznizipli@me.com>
2020-03-03 02:57:58 +0300

    docs(index): update documentation

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,12 @@ export default class Client {
   private clientSecret: string;
   private redirectUri: string;
 
+  /**
+   * @constructor
+   * @param clientId Client id
+   * @param clientSecret Client secret
+   * @param redirectUri Redirect url
+   */
   constructor(clientId: string, clientSecret: string, redirectUri: string) {
     this.requester = axios.create({
       baseURL: 'https://api.zeplin.dev/v1',
@@ -20,6 +26,11 @@ export default class Client {
     this.redirectUri = redirectUri;
   }
 
+  /**
+   * Get access token of a user.
+   * @param code Code retrieved from the redirect url's response
+   * @returns Promise<Token>
+   */
   async getToken(code: string): Promise<Token> {
     const response: AxiosResponse<Token> = await this.requester.post<Token>(`/oauth/token`, {
       grant_type: 'authorization_code',
@@ -32,6 +43,11 @@ export default class Client {
     return response.data;
   }
 
+  /**
+   * Refresh your access token, but validate if refresh_token is valid.
+   * @param token Refresh token
+   * @returns Promise<Token>
+   */
   async refreshToken(token: string): Promise<Token> {
     const response: AxiosResponse<Token> = await this.requester.post<Token>(`/oauth/token`, {
       grant_type: 'authorization_code',
@@ -44,6 +60,11 @@ export default class Client {
     return response.data;
   }
 
+  /**
+   * Get current user properties.
+   * @param accessToken Access token
+   * @returns Promise<User>
+   */
   async getCurrentUser(accessToken: string): Promise<User> {
     const response: AxiosResponse<User> = await this.requester.get<User>('/users/me', {
       headers: this.prepareHeaders(accessToken),
@@ -52,6 +73,12 @@ export default class Client {
     return response.data;
   }
 
+  /**
+   * Get currenct projects of the user
+   * @param accessToken Access token
+   * @param pagination Pagination fields including limit and offset.
+   * @returns Promise<[Project]>
+   */
   async getProjects(accessToken: string, pagination: Pagination): Promise<[Project]> {
     const response: AxiosResponse<[Project]> = await this.requester.get<[Project]>('/projects', {
       headers: this.prepareHeaders(accessToken),
@@ -61,6 +88,12 @@ export default class Client {
     return response.data;
   }
 
+  /**
+   * Retrieves a specific project
+   * @param accessToken Access token
+   * @param projectId Project id
+   * @returns Promise<Project>
+   */
   async getProject(accessToken: string, projectId: string): Promise<Project> {
     const response: AxiosResponse<Project> = await this.requester.get<Project>(
       `/projects/${projectId}`,
@@ -72,6 +105,13 @@ export default class Client {
     return response.data;
   }
 
+  /**
+   * Retrieve project's members
+   * @param accessToken Access token
+   * @param projectId Project id
+   * @param pagination Pagination fields including limit and offset.
+   * @returns Promise<[Member]>
+   */
   async getProjectMembers(
     accessToken: string,
     projectId: string,
@@ -88,6 +128,12 @@ export default class Client {
     return response.data;
   }
 
+  /**
+   * Get screens of a specific project
+   * @param accessToken Access token
+   * @param projectId Project id
+   * @param pagination Pagination fields including limit and offset.
+   */
   async getProjectScreens(
     accessToken: string,
     projectId: string,
